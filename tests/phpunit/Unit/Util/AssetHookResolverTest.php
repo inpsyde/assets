7725970eea031d70bc877ea8029225e82f116974
chrico <c.leucht@inpsyde.com>
2021-08-04 09:41:42 +0200

    AssetHookResolver // add Asset::HOOK_ACTIVATE to last hook. Add unit tests.

--- tests/phpunit/Unit/Util/AssetHookResolverTest.php
+++ tests/phpunit/Unit/Util/AssetHookResolverTest.php
@@ -20,7 +20,6 @@ use Inpsyde\WpContext;
 
 class AssetHookResolverTest extends AbstractTestCase
 {
-
     /**
      * @test
      */
@@ -46,6 +45,7 @@ class AssetHookResolverTest extends AbstractTestCase
             static::assertSame([], $hookResolver->resolve());
         }
     }
+
     /**
      * @test
      */
@@ -57,7 +57,6 @@ class AssetHookResolverTest extends AbstractTestCase
         static::assertSame([Asset::HOOK_ACTIVATE], $hookResolver->resolve());
     }
 
-    /**
     /**
      * @test
      */
@@ -100,5 +99,48 @@ class AssetHookResolverTest extends AbstractTestCase
             ],
             $hookResolver->resolve()
         );
+    }
+
+    /**
+     * @test
+     * @dataProvider provideLastHook
+     */
+    public function testResolveLastHook(string $currentContext, $expected): void
+    {
+        $context = WpContext::new()->force($currentContext);
+        $hookResolver = new AssetHookResolver($context);
+
+        static::assertSame(
+            $expected,
+            $hookResolver->lastHook()
+        );
+    }
+
+    public function provideLastHook(): \Generator
+    {
+        yield "not matching" => [
+            WpContext::AJAX,
+            null,
+        ];
+
+        yield "login" => [
+            WpContext::LOGIN,
+            Asset::HOOK_LOGIN,
+        ];
+
+        yield "frontend" => [
+            WpContext::FRONTOFFICE,
+            Asset::HOOK_FRONTEND,
+        ];
+
+        yield "backend" => [
+            WpContext::BACKOFFICE,
+            Asset::HOOK_BACKEND,
+        ];
+
+        yield "wp-activate.php" => [
+            WpContext::WP_ACTIVATE,
+            Asset::HOOK_ACTIVATE,
+        ];
     }
 }
