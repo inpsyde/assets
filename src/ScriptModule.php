1fe466bd3e71d241a1c4a95b460bbfd089375394
guido <dev@guidoscialfa.com>
2025-10-31 08:56:17 +0100

    Add support for sharing data in Script Modules and improve documentation

--- src/ScriptModule.php
+++ src/ScriptModule.php
@@ -11,6 +11,37 @@ class ScriptModule extends BaseAsset implements Asset
     use DependencyExtractionTrait;
 
     /**
+     * Data which will be added via ...
+     *      - WP_Script::add_data()
+     *      - WP_Style::add_data()
+     *
+     * @var array<string, mixed>
+     */
+    protected array $data = [];
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function data(): array
+    {
+        return $this->data;
+    }
+
+    /**
+     * Allows to set additional data via WP_Script::add_data() or WP_Style::add_data().
+     *
+     * @param array<string, mixed> $data
+     *
+     * @return static
+     */
+    public function withData(array $data): Asset
+    {
+        $this->data = array_merge($this->data, $data);
+
+        return $this;
+    }
+
+    /**
      * {@inheritDoc}
      */
     protected function defaultHandler(): string
